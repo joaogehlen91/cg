de4f710e1eb4a0c91a872fcfb93ac0d4bc2860e6
João Gehlen <joaogehlen91@gmail.com>
2016-10-05 18:01:42 -0300

    tentando fazer o trabalho de rasterizacao

--- trabalho1.py
+++ trabalho1.py
@@ -1,10 +1,28 @@
-import numpy
+matriz = []
+for i in xrange(10):
+    linha = []
+    for j in xrange(10):
+        linha.append(' ')
+    matriz.append(linha)
 
-arestas = []
+
+class Aresta:
+    def __init__(self, yIni, yFim, x, dx):
+        self.yIni = yIni
+        self.yFim = yFim
+        self.x = x
+        self.dx = dx
+
+
 
 vetores = [(20, 8), (2, 45), (80, 65), (97, 60)]
+#pol = v1->v2 v1->v3 v2->v4 v3->v4
 
-matriz = 
+ymax = 65
+ymin = 8
 
-for i, j in vetores:
-	
+# yini, yfim, x, dx
+arestas = [(8, 45, 20, 0), (8, 65, 20, 0), (45, 65, 2, 0), (65, 60, 80, 0)]
+
+LAA = [(8, 45, 20, 0), (8, 45, 20, 0), (45, 65, 2, 0), (65, 60, 80, 0)]
+
